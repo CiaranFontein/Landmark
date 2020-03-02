cfbd65e6ee48606870d7fbd53f40e8be3ad24097
Ciaran Fontein <ciaranfontein@gmail.com>
2020-03-02 13:17:26 -0800

    LIFE has parallax effect

--- src/components/LandmarkLife/index.js
+++ src/components/LandmarkLife/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { Parallax } from "react-scroll-parallax";
 import theme from "../../styles";
+import "./styles.css";
+
 const {
   colours: { primary, secondary },
   layout: { flexCenter }
 } = theme;
-export const StyledLandmarkLife = styled.div`
+
+const StyledLandmarkLife = styled.div`
   height: 450px;
   width: 100%;
   ${flexCenter}
   flex-direction: column;
 `;
 
-export const HeaderText = styled.h3`
+const HeaderText = styled.h3`
   letter-spacing: 3px;
   font-size: 0.9rem;
   font-weight: 400;
@@ -20,12 +24,10 @@ export const HeaderText = styled.h3`
   padding-bottom: 60px;
 `;
 
-export const Life = styled.div`
-  font-family: HelveticaNeue-UL, HelveticaNeue-Light, Helvetica, Arial,
-    sans-serif;
+const Life = styled.div`
   font-weight: 200;
   font-size: 6rem;
-  letter-spacing: 25px;
+  letter-spacing: 10px;
   color: ${primary};
 `;
 
@@ -33,7 +35,20 @@ const LandmarkLife = () => {
   return (
     <StyledLandmarkLife>
       <HeaderText>A Landmark</HeaderText>
-      <Life>Life</Life>
+      <Life className="centeredChildren">
+        <Parallax className="ultraLight" y={[20, 0]} x={[-60, -60]}>
+          L
+        </Parallax>
+        <Parallax className="ultraLight" y={[140, 0]} x={[-20, -20]}>
+          i
+        </Parallax>
+        <Parallax className="life" y={[100, 0]} x={[20, 20]}>
+          f
+        </Parallax>
+        <Parallax className="life" y={[50, 0]} x={[60, 60]}>
+          e
+        </Parallax>
+      </Life>
     </StyledLandmarkLife>
   );
 };
